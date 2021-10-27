c9f13acaa9d5350a5a4d63910f02fc8ef8d8c2c2
tianxie648 <nsdxietian@gmail.com>
2021-10-28 00:05:16 +0100

    Add summary of Number of Magazines (8/17 Investment)

--- chs2010.R
+++ chs2010.R
@@ -140,13 +140,14 @@ list.sumtables[[length(list.covar)]]
 #'  e) Number of Push/Pull Toys
 #'  f) How Often Child Eats With Mom/Dad
 #'  g) How Often Mom Talks to Child From Work
+#'  h) Number of Magazines
 
 
-list.covar        <- c("inv01","inv02","inv03","inv05","inv06","inv09","inv10")
+list.covar        <- c("inv01","inv02","inv03","inv05","inv06","inv09","inv10","inv12")
 labels.list.covar <- c("How Often Child Gets Out of House","Number of Books",
                        "How Often Mom Reads to Child","Number of Soft/Role Play Toys",
                        "Number of Push/Pull Toys","How Often Child Eats With Mom/Dad",
-                       "How Often Mom Talks to Child From Work")
+                       "How Often Mom Talks to Child From Work","Number of Magazines")
 
 #'  Step 2. Remove observations with -100 scores. This operation returns a list of data.frames.
 #'  The j-th element in the list is a data.frame excluding -100 scores for the j-th variable
