b5c282cb14ca6c165552fc7fc275a0b2f54832dc
tianxie648 <nsdxietian@gmail.com>
2021-11-15 01:29:05 +0000

    Transition function added

--- UT2.R
+++ UT2.R
@@ -73,3 +73,70 @@ UT2 <- function(n,m,a,k=2,P,f,
 
 
 
+#' @title Transition Function
+#' 
+#' @description The transition function of factors in (4.1) (CHS2010). The innovations are not included.
+#' 
+#' @param n     Integer. The number of factors.
+#' @param s     Indicator of the stage. 
+#' @param a     Numeric. A column vector of the input factors.
+#' @param G     List. A list of (n*n) matrices, coefficients for different stages. The (i,j) entry of a matrix is the coefficient of the j-th factor in the i-th factor's transition function.
+#' @param phi   List. A list of column vectors of the elasticity parameter for different stages. The i-th row of a column is for the i-th factor's transition function.
+#' @param alpha Matrix. An (nanch*n) matrix of the anchoring parameter, where nanch is the number of anchoring equations, which is 1 in CHS2010. The i-th row is for the i-th factor's transition function.
+#' @param rho   List. A list of column vectors of parameters for nonlinear transition. The i-th row of a column is for the i-th factor's transition function.
+#' @param const List. A list of column vectors of constants in law of motion. The i-th row of a column is for the i-th factor's transition function
+#' 
+#' @return Numeric. A column vector of factors.
+#' 
+#' @references 
+#' Supplement to "Estimating the Technology of Cognitive and Noncognitive Skill Formation": Appendix (Econometrica, Vol. 78, No.3, May 2010, 883-931)
+#' @keywords 
+#' @import
+
+
+transition <- function(n,s,a,G,phi,alpha,rho,const){
+  
+  #Get values for stage s
+  G     <- G[[s]]
+  phi   <- phi[[s]]
+  rho   <- rho[[s]]
+  const <- const[[s]]
+  
+  #Get the relevant values from alpha, the first two rows are the anchor parameter, and the rest are constant 1
+  alpha <- rbind(matrix(alpha[1,1:2]),matrix(rep(1,n-2)))
+  
+  #Transforming the factors, as factors are saved in the logarithm format
+  b <- exp(phi * alpha * a)
+  
+  #Computing the first two factors
+  trans1 <- const[1:2] / alpha[1:2] + (rho[1:2]/(phi[1:2]*alpha[1:2])) * log(G[1:2,] %*% matrix(b))
+  
+  #Computing the last factors
+  trans <- const + G %*% matrix(a)
+  trans[1:2] <- trans1
+  
+  return(trans)
+}
+
+
+
+#' @title Stage Function
+#' 
+#' @description The stage function returns the corresponding developmental stage for each year.
+#' 
+#' @param t     Integer. The year variable
+#' 
+#' @return Integer. The developmental stage.
+#' 
+#' @references 
+#' Supplement to "Estimating the Technology of Cognitive and Noncognitive Skill Formation": Appendix (Econometrica, Vol. 78, No.3, May 2010, 883-931)
+#' @keywords 
+#' @import
+
+stage <- function(t){
+}
+
+
+
+
+
