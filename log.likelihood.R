8129a792dccba417e510caad7a5e625dd449de77
Mauricio Olivares <maurice.olivares@gmail.com>
2021-12-16 11:58:00 +0000

    Modification of input section
    The new function only takes two arguments: long vector of parameters, and data. The long vector of parameters pools arrays gamma, phi, and delta.eta into one vector psi. This way we simplify the optim function needed for the MLE

--- log.likelihood.R
+++ log.likelihood.R
@@ -2,12 +2,8 @@
 #' 
 #' @description Implementation of Nonlinear Filtering. Our goal is to obtain the log of the likelihood function (A6.10) in Appendix A6 in CHS(2010). We use the Schweppe Decomposition of the likelihood function.
 #' 
+#' @param psi Numeric. Vector of dimension (k x N.theta x S + k x S + k x S), where k is the skill (cognitive or non-cognitive), N.theta is the number of latent coefficients, and S is the number of stages in childhood developent. It is worth mentioning that the order in psi matters: the first (k x N.theta x S) entries are \eqn{(\gamma_{1,C,1},\gamma_{1,N,1},\gamma_{2,C,1},\gamma_{2,N,1},\dots,\gamma_{5,C,2},\gamma_{5,N,2})}. The next \eqn{k\times S} entries are \eqn{(\phi_{C,1},\phi_{N,1},\phi_{C,2},\phi_{N,2})}, and the last \eqn{k\times S} entries are \eqn{(\delta^2_{C,1},\delta^2_{N,1},\delta^2_{C,2},\delta^2_{N,2})}. 
 #' @param Z Numeric. Matrix of observed data. Each row represents an individual.
-#' @param a0 Numeric. Vector of initial conditions corresponding to \eqn{\mathbb{E}_0(\theta_0)}. 
-#' @param P0 Numeric. Matrix of initial conditions corresponding to \eqn{\mathbb{V}_0(\theta_0)}.
-#' @param gamma Numeric. Vector of size \eqn{N_\theta} with latent factor coefficients \eqn{\gamma_1,k,s,\dots,\gamma_5,k,s}, for \eqn{k\in\{C,N\}}. The vector must satisfy \eqn{\gamma_{j,k,s}\ge0} and \eqn{\sum_j \gamma_{j,k,s}=1}.
-#' @param phi Numeric. Vector of complementarity parameters \eqn{\phi_{k,s}}, \eqn{k\in\{C,N\}}.
-#' @param delta.eta Numeric. Variance of cognitive and noncognitive shocks \eqn{\eta_{k,t}\sim\mathcal{N}(0,\delta^2_{\eta,s})}. 
 #' @param ... Extra arguments passed to the functions.
 #' 
 #' @return Numeric. The log likelihood function.
@@ -20,12 +16,35 @@
 #' 
 
 
-log.likelihood <- function(gamma,phi,delta.eta,Z,a0,P0,...){
+log.likelihood <- function(psi,Z,...){
+  
+  # -------------------------------------------------------------------------------- #
+  # Rearrange the parameters to feed the functions. Let S be the number of stages in
+  # childhood development. Then, the functions are given by
+  #
+  # phi:       Array of dimensions k*1*S. For S=s, phi[,,s] is the 
+  #            Vector of complementarity parameters \eqn{\phi_{k,s}}, \eqn{k\in\{C,N\}}.
+  # gamma:     Array of dimensions k*N.theta*S. For S=s, gamma[,,s] is the matrix of
+  #            latent coefficients \eqn{\gamma_1,k,s,\dots,\gamma_5,k,s}, for \eqn{k\in\{C,N\}}. 
+  #            The row-vector must satisfy \eqn{\gamma_{j,k,s}\ge0} and \eqn{\sum_j \gamma_{j,k,s}=1}.
+  # delta.eta  Variance of cognitive and noncognitive shocks \eqn{\eta_{k,t}\sim\mathcal{N}(0,\delta^2_{\eta,s})}.
+  phi        <- array(psi[(N.theta*k*S+1):(N.theta*k*S+k*S)], dim = c(k,1,S))
+  gamma      <- array(psi[1:(N.theta*k*S)], dim = c(k,N.theta,S))
+  delta.eta  <- array(psi[tail(psi,k*S)], dim = c(k,1,S))
+  
+  # ------------------------------------------------ #
+  # Initial moment conditions
+  # a0:  Vector of initial conditions corresponding
+  #      to \eqn{\mathbb{E}_0(\theta_0)}. 
+  # P0:  Matrix of initial conditions corresponding 
+  #      to \eqn{\mathbb{V}_0(\theta_0)}.
+  # ------------------------------------------------ #
+  a0 <- rep(1,N.theta)
+  P0 <- diag(1,ncol = N.theta)
   
   # ----------------------------------- #
   # Define the dimensions of parameters
   N.z <- ncol(Z)
-  N.theta <- length(a0)
   T.sample <- nrow(Z)
   # ----------------------------------------- #
   # Define the matrices that store the values.
