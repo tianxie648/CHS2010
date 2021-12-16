f128dfaa4c052c5833b07a5a80542771e98a9e1f
tianxie648 <nsdxietian@gmail.com>
2021-12-16 12:24:42 +0000

    Number of measurements added

---
+++ temp_tian.R
@@ -0,0 +1,14 @@
+#' Number of measurements
+#' Each column is the number of measurements for each period t
+#' Each row is by order: Child Cognitive, Child Noncognitive, Investment, Parental Cognitive, Parental Noncognitive
+M <- matrix(c(3,2,7,6,12,
+              3,5,7,6,12,
+              2,8,6,6,12,
+              4,5,14,6,12,
+              3,5,11,6,12,
+              3,5,11,6,12,
+              3,5,10,6,12,
+              3,5,10,6,12),nrow = 5)
+
+
+
