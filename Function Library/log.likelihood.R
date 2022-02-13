748244765318c6127c72c3ac1f121fc458eda865
tianxie648 <nsdxietian@gmail.com>
2022-02-13 22:23:56 +0000

    Modified the inputs
    Added Time as an input. Change the input Z as a reshaped list, with different measurements separated.

--- Function Library/log.likelihood.R
+++ Function Library/log.likelihood.R
@@ -2,11 +2,12 @@
 #' 
 #' @description Implementation of Nonlinear Filtering. Our goal is to obtain the log of the likelihood function (A6.10) in Appendix A6 in CHS(2010). We use the Schweppe Decomposition of the likelihood function.
 #' 
-#' @param Z Numeric. Matrix of observed data. Each row represents an individual.
-#' @param a0 Numeric. Vector of initial conditions corresponding to \eqn{\mathbb{E}_0(\theta_0)}. 
-#' @param P0 Numeric. Matrix of initial conditions corresponding to \eqn{\mathbb{V}_0(\theta_0)}.
-#' @param gamma Numeric. Vector of size \eqn{N_\theta} with latent factor coefficients \eqn{\gamma_1,k,s,\dots,\gamma_5,k,s}, for \eqn{k\in\{C,N\}}. The vector must satisfy \eqn{\gamma_{j,k,s}\ge0} and \eqn{\sum_j \gamma_{j,k,s}=1}.
-#' @param phi Numeric. Vector of complementarity parameters \eqn{\phi_{k,s}}, \eqn{k\in\{C,N\}}.
+#' @param Z         List. The first object is the matrix of observed data. Each row represents an individual. The rest objects are Z.1.C.M, Z.1.N.M, Z.2.M, Z.3.C, Z.3.N.
+#' @param Time      Integer. Number of time periods. 
+#' @param a0        Numeric. Vector of initial conditions corresponding to \eqn{\mathbb{E}_0(\theta_0)}. 
+#' @param P0        Numeric. Matrix of initial conditions corresponding to \eqn{\mathbb{V}_0(\theta_0)}.
+#' @param gamma     Numeric. Vector of size \eqn{N_\theta} with latent factor coefficients \eqn{\gamma_1,k,s,\dots,\gamma_5,k,s}, for \eqn{k\in\{C,N\}}. The vector must satisfy \eqn{\gamma_{j,k,s}\ge0} and \eqn{\sum_j \gamma_{j,k,s}=1}.
+#' @param phi       Numeric. Vector of complementarity parameters \eqn{\phi_{k,s}}, \eqn{k\in\{C,N\}}.
 #' @param delta.eta Numeric. Variance of cognitive and noncognitive shocks \eqn{\eta_{k,t}\sim\mathcal{N}(0,\delta^2_{\eta,s})}. 
 #' @param ... Extra arguments passed to the functions.
 #' 
@@ -20,28 +21,28 @@
 #' 
 
 
-log.likelihood <- function(gamma,phi,delta.eta,Z,a0,P0,...){
+log.likelihood <- function(gamma,phi,delta.eta,Z,Time,a0,P0,...){
   
   # ----------------------------------- #
   # Define the dimensions of parameters
-  N.z <- ncol(Z)
+  N.z <- ncol(Z[[1]])
   N.theta <- length(a0)
-  T.sample <- nrow(Z)
+  N.Sample <- nrow(Z[[1]])
   # ----------------------------------------- #
   # Define the matrices that store the values.
-  PP <- array(rep(NA, T.sample*N.theta*N.theta), dim=c(T.sample, N.theta, N.theta))
-  aa <- matrix(NA,ncol = N.theta, nrow = T.sample)
-  pred.error <- matrix(NA,ncol = N.z, nrow = T.sample)
-  F.inverse  <- array(rep(NA, T.sample*N.z*N.z), dim=c(T.sample, N.z, N.z))
+  PP <- array(rep(NA, N.Sample*N.theta*N.theta), dim=c(N.Sample, N.theta, N.theta))
+  aa <- matrix(NA,ncol = N.theta, nrow = N.Sample)
+  pred.error <- matrix(NA,ncol = N.z, nrow = N.Sample)
+  F.inverse  <- array(rep(NA, N.Sample*N.z*N.z), dim=c(N.Sample, N.z, N.z))
   # Store vectors for Schweppe Decomposition.
-  determ.t <- rep(NA,T.sample)
-  weighted.error <- rep(NA,T.sample)
+  determ.t <- rep(NA,N.Sample)
+  weighted.error <- rep(NA,N.Sample)
   
   # -------------------------------------------------------------- #
   # Initial conditions
   moments <- UT2(N.theta,a0,P0,f="no.anchor",h="linear",delta.eta=delta.eta,gamma=gamma,phi=phi)
   
-  for (t in 1:T.sample) {
+  for (t in 1:N.Sample) {
     
     # Prediction errors
     pred.error[t,] <- Z[t,]-moments$`y.hat_t`
@@ -61,7 +62,7 @@ log.likelihood <- function(gamma,phi,delta.eta,Z,a0,P0,...){
   # We exploit normality and the Kalman Filter structure to
   # implement the Schweppe Decomposition of the likelihood function
   
-  log.lik <- (N.z*T.sample/2)*log(2*pi)+(1/2)*sum(log.det)+(1/2)*sum(weighted.error)
+  log.lik <- (N.z*N.Sample/2)*log(2*pi)+(1/2)*sum(log.det)+(1/2)*sum(weighted.error)
   return(-log.lik)
 }
 
