1af1bf5b932c7371fa95f9720590750cc9ccab61
tianxie648 <nsdxietian@gmail.com>
2022-03-11 15:10:38 +0000

    A function to compute factor loadings is added
    The function Measurement() is finished

--- Function Library/Measurement.R
+++ Function Library/Measurement.R
@@ -6,7 +6,7 @@
 #' @param M    Numeric. matrix of dimension 5xTime. The number of measurements for each factor at each period. Each column of the matrix represents each period. Each row is by order: Child Cognitive, Child Noncognitive, Investment, Parental Cognitive, Parental Noncognitive
 #' @param ... Extra arguments.
 #' 
-#' @return 
+#' @return A   List. The estimated factor loadings. A[[i]][[t]][,k] is for factor i, time t, the k-th measurement, and is a column \eqn{(\mu_k,\alpha_k)^T}.
 #' 
 #' @author 
 #' @references 
@@ -23,22 +23,28 @@ Measurement <- function(Z, M){
   # (Mean of abilities is zero) 
   # - Identify the constant
   
-  # Estimate the constant:
-  mu <- unname(colMeans(Z[[1]],dims=1))
-  
-  
-  
   # Normalization condition 2: \alpha_{a,k,t,1} = 0 
   # (The first measurements have unit coefficient) 
   #- Identify coefficients
   
-  # Generate a list to store coefficients
-  # e.g. A[[1]] is a list for all coefficients for cognitive
-  # A[[1]][[1]] is a vector for all coefficients for cognitive in time = 1
-  A <- lapply(1:5, function(x) lapply(1:Time, function(t) rep(1,M[x,t])))
-  
   # Reshape the data
-  Z.time <- lapply(1:5, function(x) lapply(1:Time, function(t) NA))
+  # Z[[x]][[t]] is the measurements for x in time t
+  Z.time <- lapply(1:5, function(x) lapply(1:Time, function(t) separate.data(Z,M,x,t)))
+  
+  # Generate a list of coefficients
+  # e.g. A[[1]] is a list for all coefficients for cognitive
+  # A[[1]][[1]] is a matrix for all coefficients for cognitive in time = 1, the k-th column (mu_k, alpha_k)
+  A <- lapply(1:5, function(x) lapply(1:(Time-1), function(t) coefficient.Z(Z.time,M,x,t)))
+  
+  # Compute the coefficients for the last period separately
+  for (i in 1:5){
+    mu.Time <- unname(colMeans(Z.time[[i]][[Time]],dims=1))
+    alpha.Time <- unlist(lapply(1:M(i,Time), function(k) cov(Z.time[[i]][[Time]][,k],Z.time[[i]][[Time-1]][,1])/ 
+                                      cov(Z.time[[i]][[Time]][,1],Z.time[[i]][[Time-1]][,1])))
+    A[[i]][[Time]] <- t(matrix(cbind(mu.Time,alpha.Time), nrow=M(i,Time)))
+  }
+  
+  return(A)
 }
 
 
@@ -51,6 +57,19 @@ separate.data <- function(Z,M,x,t){
   
   Z.x.t <- Z[[x+1]][,start:end]
   return(Z.x.t)
+}
+
+coefficient.Z <- function(Z.time,M,x,t){
+  
+  # From reshaped data Z.time, calculate the coefficient for x in time t
+  
+  mu <- unname(colMeans(Z.time[[x]][[t]],dims=1))
+  
+  alpha <- unlist(lapply(1:M(x,t), function(k) cov(Z.time[[x]][[t]][,k],Z.time[[x]][[t+1]][,1])/ 
+                           cov(Z.time[[x]][[t]][,1],Z.time[[x]][[t+1]][,1])))
+  mu.alpha <- t(matrix(cbind(mu,alpha),nrow=M(x,t)))
+  
+  return(mu.alpha)
 }
 
 
