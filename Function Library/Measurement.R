d5851328939fc2d8255d208a2e400959ed5952fe
tianxie648 <nsdxietian@gmail.com>
2022-03-11 14:14:04 +0000

    Added a function reshaping data

--- Function Library/Measurement.R
+++ Function Library/Measurement.R
@@ -17,6 +17,8 @@
 
 Measurement <- function(Z, M){
   
+  Time <- dim(M)[2]
+  
   # Normalization condition 1: E(\theta) = 0 
   # (Mean of abilities is zero) 
   # - Identify the constant
@@ -25,15 +27,30 @@ Measurement <- function(Z, M){
   mu <- unname(colMeans(Z[[1]],dims=1))
   
   
-  # Normalization condition 2: \alpha_{1,C,t,1} = 0 
-  # (The first measurements for cognitive have unit coefficient) 
+  
+  # Normalization condition 2: \alpha_{a,k,t,1} = 0 
+  # (The first measurements have unit coefficient) 
   #- Identify coefficients
   
   # Generate a list to store coefficients
+  # e.g. A[[1]] is a list for all coefficients for cognitive
+  # A[[1]][[1]] is a vector for all coefficients for cognitive in time = 1
+  A <- lapply(1:5, function(x) lapply(1:Time, function(t) rep(1,M[x,t])))
   
-  
+  # Reshape the data
+  Z.time <- lapply(1:5, function(x) lapply(1:Time, function(t) NA))
 }
 
 
+separate.data <- function(Z,M,x,t){
+  
+  # From data Z, pick the measurements for x in time t
+  
+  end <- sum(M[x,1:t])
+  start <- end - M[x,t] + 1
+  
+  Z.x.t <- Z[[x+1]][,start:end]
+  return(Z.x.t)
+}
 
 
